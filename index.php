85ab657b0afb2dd3ce09bd2bd614c1a0cc1f11d9
MFinke <mirkofinke@gmail.com>
2013-12-14 22:08:08 +0100

    PHP-Datei erzeugt

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+
+
+  <title>HTML</title>
+  <meta name="description" content="">
+  <meta name="author" content="Mirko Finke">
+
+
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+</head>
+
+<body>
+  <div>
+    <header>
+      <h1>HTML</h1>
+    </header>
+    <nav>
+      <p><a href="/">Home</a></p>
+      <p><a href="/contact">Contact</a></p>
+    </nav>
+
+    <div>
+      
+    </div>
+
+    <footer>
+     <p>&copy; Copyright  by Mirko Finke</p>
+    </footer>
+  </div>
+</body>
+</html>
